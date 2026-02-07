3f6523189a767fce5a67d9d24819b5cbfc7075d1
Ezefernandezyf <ezefernandezyf@gmail.com>
2026-02-06 21:35:53 -0300

    feat(routes): update favorites route to use FavoritesPage component

--- src/pages/AppRoutes/AppRoutes.tsx
+++ src/pages/AppRoutes/AppRoutes.tsx
@@ -3,6 +3,7 @@ import { HomePage } from '../HomePage/HomePage';
 import { Layout } from '../../components';
 import { NotFoundPage } from '../NotFoundPage/NotFoundPage';
 import SearchPage from '../SearchPage/SearchPage';
+import FavoritesPage from '../FavoritesPage/FavoritesPage';
 
 
 export const AppRoutes = (): React.JSX.Element => {
@@ -12,7 +13,7 @@ export const AppRoutes = (): React.JSX.Element => {
         <Route index element={<Navigate to="home" replace />} />
         <Route path="home" element={<HomePage />} />
         <Route path="search" element={<SearchPage />} />
-        <Route path="favorites" element={<h1>este es el favorites</h1>} />
+        <Route path="favorites" element={<FavoritesPage />} />
         <Route path="not-found" element={<NotFoundPage />} />
         <Route path="*" element={<Navigate to="/not-found" replace />} />
       </Route>
